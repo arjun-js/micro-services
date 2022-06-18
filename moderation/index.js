f0b590f121cc2e4620a70a074d19ccefd4cb766d
Arjun <Arjun>
2022-06-18 17:02:57 +0530

    initial changes

---
+++ moderation/index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.post('/events', async (req, res)=>{
+    const {type, data} = req.body;
+    await handleEvent(type, data);
+    res.send({});
+});
+
+async function handleEvent(type, data){
+    if(type==='CommentCreated'){
+        const status = data.content && data.content.includes('orange') ? 'rejected' : 'approved';
+        await axios.post('http://localhost:4005/events', {
+         type : 'CommentModerated',
+         data : {
+            ...data,
+            status: status 
+         }
+        });
+    } 
+}
+
+app.listen(4003, async ()=>{
+    console.log('Listening on 4003');
+
+    try{
+    const res = await axios.get('http://localhost:4005/events');
+
+    for(let event of res.data){
+        console.log('processing event', event.type);
+    handleEvent(event.type, event.data);
+    }
+}
+catch(err){
+    console.log(err);
+
+}
+});
